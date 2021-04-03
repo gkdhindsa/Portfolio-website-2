7ca64ef95c4f3ce4b0307d171533d625c66760d5
Gurnehmat K. Dhindsa <gurnehmat.dhindsa@gmail.com>
2021-04-03 13:18:41 +0530

    navbar update, slider, home text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
 var rects=document.querySelectorAll('.rainbow')
-var colors=['#9400D3', '#4B0082','#0000FF', 
-'#00FF00', '#ffff00', '#ff7f00', '#ff0000']
+var colors=['whitesmoke', 'gainsboro','#c8c8c8', 
+'#b0b0b0', '#989898']
 rects.forEach((element, index)=>{
   element.addEventListener('mouseover', function(){
     element.style.backgroundColor=colors[index]
     myVar = setTimeout(function()       {element.style.backgroundColor='#fff'},1000);
     
   })
+})
+
+/*Slider*/
+let btn=document.querySelector(".circle");
+let whole=document.querySelector(".outer");
+whole.addEventListener("click", function(){
+  if(btn.classList.contains('left'))
+    btn.style.marginLeft="45px";
+  else
+     btn.style.marginLeft="0px";
+  btn.classList.toggle('left')
+    
 })
